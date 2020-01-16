2c15c9e0ec7f7e25d09590ddf0969c42ff6fff86
stranger900 <strangerua900@gmail.com>
2020-01-16 10:13:42 +0200

    dicee

---
+++ dicee/lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var leftDiceNumber = 1;
+  var rightDiceNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Center(
+        child: Row(
+          children: <Widget>[
+            Expanded(child:
+            FlatButton(
+                onPressed: () {
+                  setState(() {
+
+                    leftDiceNumber = 2;
+                  });
+                },
+                child: Image.asset('images/dice$leftDiceNumber.png')),
+            ),
+            Expanded(
+              child: FlatButton(
+                  onPressed: () {
+                    leftDiceNumber = 2;
+                  },
+                  child: Image.asset('images/dice$rightDiceNumber.png')),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
