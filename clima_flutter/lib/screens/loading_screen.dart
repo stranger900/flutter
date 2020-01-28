aacd5db8d4e2795f9d2648c2c3bf28525d59aa8b
stranger900 <strangerua900@gmail.com>
2020-01-28 13:54:36 +0200

    clima

--- clima_flutter/lib/screens/loading_screen.dart
+++ clima_flutter/lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import '../services/location.dart';
+import 'package:http/http.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -8,11 +9,24 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
 
-  void getLocation() async{
-    Position position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+  @override
+  void initState(){
+    super.initState();
+    getLocation;
   }
 
-  @override
+  void getLocation() async{
+    Location location = Location();
+    await location.getCurrentLocation();
+    print(location.latitude);
+    print(location.longitude);
+  }
+  
+  void getData() async{
+    Response response = await get('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
+    print(response.body);
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
