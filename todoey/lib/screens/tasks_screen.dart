56edd1f9fc271d6237a7501e0129eb0d2dc5e172
stranger900 <36480357+stranger900@users.noreply.github.com>
2020-03-02 22:48:28 +0200

    todoey

--- todoey/lib/screens/tasks_screen.dart
+++ todoey/lib/screens/tasks_screen.dart
@@ -6,29 +6,47 @@ class TasksScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
-      body: Container(
-        padding: EdgeInsets.only(bottom: 30, right: 30, left: 30, top: 60),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            CircleAvatar(
-              child: Icon(Icons.list, size: 30,color: Colors.lightBlueAccent,),
-              backgroundColor: Colors.white,
-              radius: 30,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.lightBlueAccent,
+        child: Icon(Icons.add),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(bottom: 30, right: 30, left: 30, top: 60),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                CircleAvatar(
+                  child: Icon(Icons.list, size: 30,color: Colors.lightBlueAccent,),
+                  backgroundColor: Colors.white,
+                  radius: 30,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Todoey',
+                  style: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.w700),
+                ),
+                Text(
+                  '12 tasks',
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                ),
+
+              ],
             ),
-            SizedBox(
-              height: 10,
+          ),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+              ),
             ),
-            Text(
-              'Todoey',
-              style: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.w700),
-            ),
-            Text(
-              '12 tasks',
-              style: TextStyle(color: Colors.white, fontSize: 18),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
