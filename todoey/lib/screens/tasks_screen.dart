846c2a8bf776ce80bc0cb67d613fa0ffb59f0f64
stranger900 <36480357+stranger900@users.noreply.github.com>
2020-03-02 18:39:29 +0200

    todoey

--- todoey/lib/screens/tasks_screen.dart
+++ todoey/lib/screens/tasks_screen.dart
@@ -3,6 +3,16 @@ import 'package:flutter/material.dart';
 class TasksScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Icon(Icons.list),
+            Text('Todoey'),
+          ],
+        ),
+      ),
+    );
   }
 }
