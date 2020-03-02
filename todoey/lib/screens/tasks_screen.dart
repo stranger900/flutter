3f558eaaadebb36a6a55fa9b57e38639fc9b0370
stranger900 <36480357+stranger900@users.noreply.github.com>
2020-03-02 21:35:47 +0200

    todoey

--- todoey/lib/screens/tasks_screen.dart
+++ todoey/lib/screens/tasks_screen.dart
@@ -1,15 +1,32 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class TasksScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
-      body: SafeArea(
+      backgroundColor: Colors.lightBlueAccent,
+      body: Container(
+        padding: EdgeInsets.only(bottom: 30, right: 30, left: 30, top: 60),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Icon(Icons.list),
-            Text('Todoey'),
+            CircleAvatar(
+              child: Icon(Icons.list, size: 30,color: Colors.lightBlueAccent,),
+              backgroundColor: Colors.white,
+              radius: 30,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'Todoey',
+              style: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.w700),
+            ),
+            Text(
+              '12 tasks',
+              style: TextStyle(color: Colors.white, fontSize: 18),
+            ),
           ],
         ),
       ),
