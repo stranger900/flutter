fdd453fbfb25af19220918d96ff03838f8fc6ff2
stranger900 <36480357+stranger900@users.noreply.github.com>
2020-03-15 16:19:02 +0200

    todoey

--- todoey/lib/screens/add_task_screen.dart
+++ todoey/lib/screens/add_task_screen.dart
@@ -6,8 +6,22 @@ class AddTaskScreen extends StatelessWidget {
     return Container(
       color: Color(0xff757575),
       child: Container(
+        padding: EdgeInsets.all(20),
         decoration: BoxDecoration(
-          color: Colors.white
+          color: Colors.white,
+          borderRadius: BorderRadius.only(topRight: Radius.circular(20), topLeft: Radius.circular(20)),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text('Add Task', textAlign: TextAlign.center,style: TextStyle(color: Colors.lightBlueAccent, fontSize: 30),),
+            TextField(autofocus: true,textAlign: TextAlign.center,),
+            FlatButton(
+                onPressed: () {},
+                child: Text('Add'),
+              color: Colors.lightBlueAccent,
+            ),
+          ],
         ),
       ),
     );
